5f56b336a81e614551922d93bcca3c898762dda0
BombingArt <74514537+BombingArt@users.noreply.github.com>
2024-05-29 03:11:12 +0300

    task 1

---
+++ connect.py
@@ -0,0 +1,14 @@
+from mongoengine import connect
+import configparser
+
+config = configparser.ConfigParser()
+config.read(r'c:\Users\ABank\Desktop\goit-web-hw-08\task_1\config.ini')
+
+mongo_user = config.get('DB', 'USER')
+mongodb_pass = config.get('DB', 'PASS')
+db_name = config.get('DB', 'DB_NAME')
+domain = config.get('DB', 'DOMAIN')
+options = config.get('DB', 'OPTIONS')
+
+connect(host=f"mongodb+srv://{mongo_user}:{mongodb_pass}@{domain}/{db_name}?{options}", ssl=True)
+
---
+++ seed.py
@@ -0,0 +1,27 @@
+import json
+import connect
+from models import Author, Quote
+
+
+with open('c:\\Users\\ABank\\Desktop\\goit-web-hw-08\\task_1\\authors.json', 'r', encoding='utf-8') as file:
+    authors_data = json.load(file)
+    for author_data in authors_data:
+        author = Author(
+            fullname=author_data['fullname'],
+            born_date=author_data['born_date'],
+            born_location=author_data['born_location'],
+            description=author_data['description']
+        )
+        author.save()
+
+with open('c:\\Users\\ABank\\Desktop\\goit-web-hw-08\\task_1\\qoutes.json', 'r', encoding='utf-8') as file:
+    quotes_data = json.load(file)
+    for quote_data in quotes_data:
+        author = Author.objects(fullname=quote_data['author']).first()
+        if author:
+            quote = Quote(
+                tags=quote_data['tags'],
+                author=author,
+                quote=quote_data['quote']
+            )
+            quote.save()
