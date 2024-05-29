e1f6356f86cecd24427442c5c714268606cc549d
BombingArt <74514537+BombingArt@users.noreply.github.com>
2024-05-29 03:15:19 +0300

    Update connect.py

--- task_1/connect.py
+++ task_1/connect.py
@@ -2,7 +2,7 @@ from mongoengine import connect
 import configparser
 
 config = configparser.ConfigParser()
-config.read(r'c:\Users\ABank\Desktop\goit-web-hw-08\task_1\config.ini')
+config.read(config.ini')
 
 mongo_user = config.get('DB', 'USER')
 mongodb_pass = config.get('DB', 'PASS')
