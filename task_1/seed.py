cec1e5f95b3ddba056c8fdb2e115cffee6d2cded
BombingArt <74514537+BombingArt@users.noreply.github.com>
2024-05-29 21:44:23 +0300

    Update seed.py

--- task_1/seed.py
+++ task_1/seed.py
@@ -14,7 +14,7 @@ with open('json/authors.json', 'r', encoding='utf-8') as file:
         )
         author.save()
 
-with open('json/qoutes.json', 'r', encoding='utf-8') as file:
+with open('json/quotes.json', 'r', encoding='utf-8') as file:
     quotes_data = json.load(file)
     for quote_data in quotes_data:
         author = Author.objects(fullname=quote_data['author']).first()
